89fabc164405e4d87b06a0067a283970864db71b
Jason <jkasari@outlook.com>
2020-12-03 16:46:16 -0800

    write function outlines

--- main.cpp
+++ main.cpp
@@ -8,13 +8,30 @@ int main() {
   srand(time(0));
 
   dice d(6);
-  for(int i = 0; i < 5; ++i) {
-    cout << d.roll() << endl;
+  for (int i = 0; i < 5; ++i) {
+    //  cout << d.roll() << endl;
   }
-  cout << "-------------" << endl;
+  // cout << "-------------" << endl;
   dice d2(24);
-  for(int i = 0; i < 5; ++i) {
-    cout << d2.roll() << endl;
+  for (int i = 0; i < 5; ++i) {
+    // cout << d2.roll() << endl;
   }
+
+  int numDice = 10;
+  dice** myDice = new dice*[numDice];
+  for (int i = 0; i < numDice; ++i) {
+    myDice[i] = new dice(i + 1);
+  }
+  for (int i = 0; i < numDice; ++i) {
+    cout << myDice[i]->roll();
+    cout << "--->" << (i + 1) << "Sided Dice" << endl;
+  }
+
+  for (int i = 0; i < numDice; ++i) {
+    delete myDice[i];
+    myDice[i] = nullptr;
+  }
+  delete[] myDice;
+  myDice = nullptr;
   return 0;
 }
