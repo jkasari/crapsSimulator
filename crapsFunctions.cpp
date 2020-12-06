5a590886c3f7d7ed0604a03062408daa3687f664
Jason <jkasari@outlook.com>
2020-12-06 10:53:56 -0800

    crapsGame creates a working arr

--- crapsFunctions.cpp
+++ crapsFunctions.cpp
@@ -3,20 +3,6 @@
 /**
  * behives like a die, use the .roll function to generate a virtual roll.
  */
-// class dice
-
-/**
- * takes the number of turns you want to track and the number of games you want
- * to simulate. it then returns the results in a int arr. This arr size is
- * decided by the number of turns you want to map.
- */
-//int** rawResults crapsGame(const int32_t numOfGames, const int32_t turnsToMap)
-
-/**
- * this takes the results genarated from |crapsGame| and displays them in a readable format.
- */
-//void displayResults(int** rawResults)
-
 class dice {
  public:
   dice(uint32_t sides) { this->sides = sides; }
@@ -25,3 +11,30 @@ class dice {
  private:
   uint32_t sides;
 };
+
+/**
+ * takes the number of turns you want to track and the number of games you want
+ * to simulate. it then returns the results in a int arr. This arr size is
+ * decided by the number of turns you want to map.
+ */
+int32_t* crapsGame(const int32_t numOfGames, const int32_t turnsToMap) {
+  dice crapsDice(12);
+  int32_t* rawResults = new int32_t[turnsToMap + 1];
+  for (int i = 0; i < numOfGames; ++i) {
+    int32_t firstRoll = crapsDice.roll();
+    if (firstRoll == 7 || firstRoll == 11) {
+      rawResults[i % turnsToMap] += 1;
+    } else {
+      rawResults[turnsToMap] += 1;
+    }
+  }
+  return rawResults;
+  delete[] rawResults;
+  rawResults = nullptr;
+}
+
+/**
+ * this takes the results genarated from |crapsGame| and displays them in a
+ * readable format.
+ */
+// void displayResults(int** rawResults)
--- main.cpp
+++ main.cpp
@@ -7,31 +7,36 @@ using namespace std;
 int main() {
   srand(time(0));
 
-  dice d(6);
-  for (int i = 0; i < 5; ++i) {
+//  dice d(6);
+//  for (int i = 0; i < 5; ++i) {
     //  cout << d.roll() << endl;
-  }
-  // cout << "-------------" << endl;
-  dice d2(24);
-  for (int i = 0; i < 5; ++i) {
+//  }
+//  // cout << "-------------" << endl;
+//  dice d2(24);
+//  for (int i = 0; i < 5; ++i) {
     // cout << d2.roll() << endl;
-  }
+//  }
+//
+//  int numDice = 10;
+//  dice** myDice = new dice*[numDice];
+//  for (int i = 0; i < numDice; ++i) {
+//    myDice[i] = new dice(i + 1);
+//  }
+//  for (int i = 0; i < numDice; ++i) {
+//    cout << myDice[i]->roll();
+//    cout << "--->" << (i + 1) << "Sided Dice" << endl;
+//  }
+//
+//  for (int i = 0; i < numDice; ++i) {
+//    delete myDice[i];
+//    myDice[i] = nullptr;
+//  }
+//  delete[] myDice;
+//  myDice = nullptr;
+//  return 0;
 
-  int numDice = 10;
-  dice** myDice = new dice*[numDice];
-  for (int i = 0; i < numDice; ++i) {
-    myDice[i] = new dice(i + 1);
+  int32_t* results = crapsGame(50, 5);
+  for(int i = 0; i < 6; ++i) {
+    cout << results[i] << endl;
   }
-  for (int i = 0; i < numDice; ++i) {
-    cout << myDice[i]->roll();
-    cout << "--->" << (i + 1) << "Sided Dice" << endl;
-  }
-
-  for (int i = 0; i < numDice; ++i) {
-    delete myDice[i];
-    myDice[i] = nullptr;
-  }
-  delete[] myDice;
-  myDice = nullptr;
-  return 0;
 }
