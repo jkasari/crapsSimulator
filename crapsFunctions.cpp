f354544832a106f8cae012c435bc7b01b42f54e5
Jason <jkasari@outlook.com>
2020-12-06 16:48:50 -0800

    crapsGame gives a slightly less wrong answer now

--- crapsFunctions.cpp
+++ crapsFunctions.cpp
@@ -45,14 +45,14 @@ int32_t* crapsGame(const int32_t numOfGames, const int32_t turnsToMap) {
   dice crapsDice2(6);
   int32_t target = 0;
   int32_t turnCounter = 0;
-  int32_t* rawResults = new int32_t[turnsToMap + 2];
+  int32_t* rawResults = new int32_t[turnsToMap + 1];
   for (int i = 0; i < numOfGames;) {
     int32_t roll = crapsDice1.roll() + crapsDice2.roll();
+    if(turnCounter == 0){
+      target = roll;
+    }
     if(turnCounter > turnsToMap){
       turnCounter = (turnsToMap + 1);
-    }
-    if(crapsTurn(roll, turnCounter, target) == 0) {
-    ++turnCounter;
     }
     if(crapsTurn(roll, turnCounter, target) == 1) {
     rawResults[turnCounter] +=1;
@@ -63,8 +63,8 @@ int32_t* crapsGame(const int32_t numOfGames, const int32_t turnsToMap) {
     turnCounter = 0;
     ++i;
     }
-    if(turnCounter == 0){
-      target = roll;
+    if(crapsTurn(roll, turnCounter, target) == 0) {
+    ++turnCounter;
     }
   }
   return rawResults;
