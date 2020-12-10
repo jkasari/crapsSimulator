1b10fcd826c62d1e1118c5e315053a62963c51cc
Jason <jkasari@outlook.com>
2020-12-09 19:56:04 -0800

    make main take args

--- main.cpp
+++ main.cpp
@@ -1,50 +1,33 @@
 #include <iostream>
+#include <iomanip>
 #include <ctime>
 #include "crapsFunctions.cpp"
 
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
   srand(time(0));
 
-//  dice d(6);
-//  for (int i = 0; i < 5; ++i) {
-    //  cout << d.roll() << endl;
-//  }
-//  // cout << "-------------" << endl;
-//  dice d2(24);
-//  for (int i = 0; i < 5; ++i) {
-    // cout << d2.roll() << endl;
-//  }
-//
-//  int numDice = 10;
-//  dice** myDice = new dice*[numDice];
-//  for (int i = 0; i < numDice; ++i) {
-//    myDice[i] = new dice(i + 1);
-//  }
-//  for (int i = 0; i < numDice; ++i) {
-//    cout << myDice[i]->roll();
-//    cout << "--->" << (i + 1) << "Sided Dice" << endl;
-//  }
-//
-//  for (int i = 0; i < numDice; ++i) {
-//    delete myDice[i];
-//    myDice[i] = nullptr;
-//  }
-//  delete[] myDice;
-//  myDice = nullptr;
-//  return 0;
 
-  const uint32_t numOfGames = 100000;
+  cout << fixed;
+  cout << setprecision(4);
 
-  uint32_t* results = crapsGame(numOfGames, 10);
-  for(int i = 0; i < 11; ++i) {
+  const uint32_t numOfGames = stol(argv[2]);
+  const uint32_t turnsToMap = stol(argv[1]);
+  const uint32_t width = 7;
+
+  uint32_t* results = crapsGame(numOfGames, turnsToMap);
+  for(int i = 0; i < (turnsToMap + 1); ++i) {
+    cout.width(width);
+    cout << right;
     cout << static_cast<double>(results[i]) / numOfGames * 100 << "%" << endl;
   }
   uint32_t total = 0;
-  for(int i = 0; i < 11; ++i) {
+  for(int i = 0; i < (turnsToMap + 1); ++i) {
     total += results[i];
   }
+  cout.width(width);
+  cout << right;
   cout << static_cast<double>(total) / numOfGames * 100 << "%" << endl;
 
   delete[] results;
