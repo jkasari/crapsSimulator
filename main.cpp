7b4e6438087d434ba71cd98715083c402c4e02b5
Jason <jkasari@outlook.com>
2020-12-09 20:29:44 -0800

    add verbage in output

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char** argv) {
     cout << endl;
     cout << " 1.) The number of turns to map." << endl;
     cout << endl;
-    cout << " 2.) The number of games to play." << endl;
+    cout << " 2.) The number of games to play." << endl << endl;
     return 0;
   }
 
@@ -24,19 +24,35 @@ int main(int argc, char** argv) {
   const uint32_t turnsToMap = stol(argv[1]);
   const uint32_t width = 7;
 
+  cout << endl << "Calculating the first (" << turnsToMap;
+  cout << ") turns of (" << numOfGames;
+  cout << ") games of Craps! . . ." << endl << endl;
+
   uint32_t* results = crapsGame(numOfGames, turnsToMap);
-  for(int i = 0; i < (turnsToMap + 1); ++i) {
+  for (int i = 0; i < (turnsToMap); ++i) {
     cout.width(width);
     cout << right;
+    cout << "Turn (" << (i + 1) << ") ";
     cout << static_cast<double>(results[i]) / numOfGames * 100 << "%" << endl;
   }
+
+  cout << endl << "After turn (" << turnsToMap << ") ";
+  cout << static_cast<double>(results[turnsToMap]) / numOfGames * 100 << "%" << endl << endl;
+
   uint32_t total = 0;
-  for(int i = 0; i < (turnsToMap + 1); ++i) {
+  for (int i = 0; i < (turnsToMap + 1); ++i) {
     total += results[i];
   }
   cout.width(width);
   cout << right;
+  cout << "Total chance for (" << numOfGames << ") games ";
   cout << static_cast<double>(total) / numOfGames * 100 << "%" << endl;
+
+  if ((static_cast<double>(total) / numOfGames * 100) < 50) {
+    cout << endl << "YOU FOOL! The house ALWAYS WINS!! SUCK IT NERD!!!!" << endl;
+  } else if ((static_cast<double>(total) / numOfGames * 100) > 50) {
+    cout << endl << "Ahhhhh looks like somebody got lucky" << endl;
+  }
 
   delete[] results;
   results = nullptr;
